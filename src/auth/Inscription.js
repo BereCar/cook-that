8ef437115bed1d4b2bcc63425af366faa4aa13cf
BereCar <berenice.cartillier@hotmail.fr>
2021-09-14 15:34:43 +0200

    progres formulaire

--- src/auth/Inscription.js
+++ src/auth/Inscription.js
@@ -1,62 +1,192 @@
-import React, {useState, useEffect} from 'react'
-import { FormControl, TextField , InputLabel, Input, Button, Icon} from '@material-ui/core';
+import { OndemandVideoOutlined } from '@material-ui/icons'
+import React, { useState, useEffect } from 'react'
 import { auth } from '../firebase'
 
-const SENDING_STEPS = {   
-    NONE: 'NONE',  
-    VALIDATION: 'VALIDATION',  
-    SEND_DATA: 'SEND_DATA',
-  }
+/**
+ * Voici les différente de ce qui se produit lors d'une inscription
+ */
+const SENDING_STEPS = {
+  /**
+   * Correspond à l'état initial, pas d'enscription envoyéc
+   */
+  NONE: 'NONE',
 
-export default()=>{
-    const [mail, setMail] = useState({ value: '', error: '' })
-    const[pwd, setPwd] = useState({ value: '', error: '' })
-    const[confirm, setConfirm]= useState({ value: '', error: '' })
-    
-    return(
-        <>      
-         
-      
-        <FormControl>
-         <InputLabel htmlFor="my-input">Email address</InputLabel>
-        <Input id="my-input-email" aria-describedby="my-helper-text"   value={mail? mail : ""} 
-            onChange={(e)=>{
-                setMail(e.target.value)
-            }}
+  /**
+   * On demande la validation des données
+   */
+  VALIDATION: 'VALIDATION',
+
+  /**
+   * On envoie les données à firebase
+   */
+  SEND_DATA: 'SEND_DATA',
+}
+
+export default () => {
+  // STATE
+  const [emailField, setEmailField] = useState({ value: '', error: '' })
+  const [passwordField, setPasswordField] = useState({ value: '', error: '' })
+  const [confirmPasswordField, setConfirmPasswordField] = useState({
+    value: '',
+    error: '',
+  })
+  const [formError, setFormError] = useState('')
+  const [step, setStep] = useState(SENDING_STEPS.NONE)
+
+  // EFFECT
+  // Première effet, la validation des données
+  useEffect(() => {
+    // Cette effet se déclenche uniquement lors de la step "VALIDATION"
+    if (step !== SENDING_STEPS.VALIDATION) return
+
+    // Validation de l'email
+    const validateEmail = () => {
+      let valid = true
+
+      if (!emailField.value) {
+        setEmailField({
+          value: emailField.value,
+          error: 'Vous devez renseigner un email',
+        })
+
+        valid = false
+      }
+
+      if (!/^[^@]+@[^.]+\.[a-zA-Z0-9]+/.test(emailField.value)) {
+        setEmailField(e) ({
+          value: passwordField.e.target.value,
+          error: 'Vous devez renseigner un email valide',
+        })
+
+        valid = false
+      }
+
+      return valid
+    }
+
+    // Validation des mots de passe
+    const validatePasswords = () => {
+      let valid = true
+
+      if (!passwordField.value) {
+        setPasswordField({
+          value: passwordField.value,
+          error: 'Vous devez renseigner un mot de passe',
+        })
+
+        valid = false
+      }
+
+      if (!confirmPasswordField.value) {
+        onEmailChange
+
+        valid = false
+      }
+
+      if (passwordField.value !== confirmPasswordField.value) {
+        setFormError('Vos deux mots de passe doivent correspondre')
+
+        valid = false
+      }
+
+      return valid
+    }
+
+    // On vide l'erreur de formulaire
+    setFormError('')
+
+    // On Attend 1 seconde
+    setTimeout(() => {
+      // On valid l'email et le mot de passe
+      let validEmail = validateEmail()
+      let validPassword = validatePasswords()
+
+      // Si c'est valid on passe à l'étape SEND_DATA
+      if (validEmail && validPassword) {
+        setStep(SENDING_STEPS.SEND_DATA)
+      } else {
+        // On retourne à l'étape NONE
+        setStep(SENDING_STEPS.NONE)
+      }
+    }, 1000)
+  }, [step])
+
+  // Second effet, l'envoie des données à firebase
+  useEffect(() => {
+    // On éxécute l'effet uniquement si l'étape est SEND_DATA
+    if (step !== SENDING_STEPS.SEND_DATA) return
+
+    const sendToFirebase = async () => {
+      try {
+        const userCredential = await auth.createUserWithEmailAndPassword(
+          emailField.value,
+          passwordField.value
+        )
+
+        console.warn(userCredential)
+
+        setStep(SENDING_STEPS.NONE)
+      } catch (e) {
+        setFormError(e.message)
+
+        setStep(SENDING_STEPS.NONE)
+      }
+    }
+
+    setTimeout(() => {
+      sendToFirebase()
+    }, 1000)
+  }, [step])
+
+  // HELPERS
+  // curried function
+  // setField :: { value :: string, error :: string } -> Void
+  // changeField :: setField -> React.Event -> Void
+  const changeField = setField => ev =>
+    setField({
+      error: '',
+      value: ev.target.value,
+    })
+
+  return (
+    <div>
+      <div>
+        <p>Votre email :</p>
+        {!!emailField.error && (
+          <p>{emailField.error}</p>
+        )}
+        <input
+          value={emailField.value}
+          // onChangeText :: String -> Void
+          onChange={changeField(setEmailField)}
         />
-         
-        </FormControl>
-        <FormControl>
-         <InputLabel htmlFor="my-input">Mot de passe : </InputLabel>
-         <TextField label="confirmation"  id="my-input-password" aria-describedby="my-helper-text" 
-                 
-                 variant='outlined' margin='normal' required
-                 name='password' label='Password' type='password'
-                 value={pwd? pwd : ""}
-                 onChange={(e) => {
-                   setPwd(e.target.value)
-                  
-                 }}                
-                 autoComplete='current-password'
-                 
-                 />
-        </FormControl>
-
-        <FormControl>
-         <InputLabel htmlFor="my-input">Confirmer votre mot de passe : </InputLabel>
-        <TextField label="confirmation"  id="my-input-confirm" aria-describedby="my-helper-text" 
-                 
-                  variant='outlined' margin='normal' required
-                  name='password' label='Password' type='password'
-                  value={confirm? confirm : ""}
-                  onChange={(e) => {
-                    setConfirm(e.target.value)                   
-                  }}                 
-                  autoComplete='current-password'                  
-                  />
-         
-        </FormControl>
-        <Button variant="contained" color="primary" onSubmit={()=>{}} >Inscription</Button>
-        </>
-    )
+      </div>
+      <div>
+        <p>Votre mot de passe :</p>
+        {!!passwordField.error && (
+          <p>{passwordField.error}</p>
+        )}
+        <input
+          value={passwordField.value}
+          onChange={changeField(setPasswordField)}
+        />
+      </div>
+      <div>
+        <p>Confirmation du mot de passe :</p>
+        {!!confirmPasswordField.error && (
+          <p >{confirmPasswordField.error}</p>
+        )}
+        <input
+          value={confirmPasswordField.value}
+          onChange={changeField(setConfirmPasswordField)}
+        />
+      </div>
+      {step === SENDING_STEPS.NONE ? (
+        <button onClick={() => setStep(SENDING_STEPS.VALIDATION)} />
+      ) : (
+        //<Loading step={step} />
+        <p>En train d'envoyer</p>
+      )}
+    </div>
+  )
 }
